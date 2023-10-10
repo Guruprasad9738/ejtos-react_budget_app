c813cbbbb02b796c3e9b7aefe6a8311fafac3339
Guruprasad9738 <guruprasadmylandlahalli@outlook.com>
2023-10-10 10:25:07 -0400

    Added Currency.js and its related changes

--- src/App.js
+++ src/App.js
@@ -6,14 +6,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
-
 // Add code to import the other components here under
-
-
 import { AppProvider } from './context/AppContext';
 import ExpenseList from './components/ExpenseList';
 import ExpenseItem from './components/ExpenseItem';
 import AllocationForm from './components/AllocationForm';
+import Currency from './components/Currency';
+
 const App = () => {
     return (
         <AppProvider>
@@ -61,7 +60,14 @@ const App = () => {
                             <div className='col-sm'>
                                 <AllocationForm />
                             </div>
-                        }        
+                        }   
+
+                        {
+                            /* Add Currency component here under */
+                            <div className='col-sm'>
+                                <Currency />
+                            </div>
+                        }       
 
                 </div>
             </div>
